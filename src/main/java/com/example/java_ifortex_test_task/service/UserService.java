c1e193b77bf8f879da724eabc5cda02675df0f75
DP <did.ink@mail.ru>
2025-06-03 01:08:38 +0300

    [UserService] - [Added] - This class contains two methods: one returns the user with the highest number of sessions; the other returns a list of users who have at least one MOBILE session.

--- src/main/java/com/example/java_ifortex_test_task/service/UserService.java
+++ src/main/java/com/example/java_ifortex_test_task/service/UserService.java
@@ -1,8 +1,9 @@
 package com.example.java_ifortex_test_task.service;
 
 import com.example.java_ifortex_test_task.dto.UserResponseDTO;
+import com.example.java_ifortex_test_task.entity.DeviceType;
+import com.example.java_ifortex_test_task.entity.User;
 import com.example.java_ifortex_test_task.mapper.UserMapper;
-import com.example.java_ifortex_test_task.repository.SessionRepository;
 import com.example.java_ifortex_test_task.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,17 +13,23 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
-    private final SessionRepository sessionRepository;
     private final UserMapper userMapper;
+
+    private static final int MOBILE_CODE = DeviceType.MOBILE.getCode();
 
     // Returns a User with the biggest amount of sessions
     public UserResponseDTO getUserWithMostSessions() {
-        return null;
+        User userWithMostSessions = userRepository.getUserWithMostSessions();
+        return userMapper.toDto(userWithMostSessions);
     }
 
     // Returns Users that have at least 1 Mobile session
     public List<UserResponseDTO> getUsersWithAtLeastOneMobileSession() {
-        return null;
+        return userRepository.getUsersWithAtLeastOneMobileSession(MOBILE_CODE)
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
     }
 }
