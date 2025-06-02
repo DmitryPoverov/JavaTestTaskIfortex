e7ce00fde387d61660e427a323b6ea3f9dad9c8c
DP <did.ink@mail.ru>
2025-06-03 01:02:48 +0300

    [SessionRepository] - [Added] - This class contains two methods: one returns the first DESKTOP session, and the other returns a list of sessions that belong to active users and ended before 2025.

--- src/main/java/com/example/java_ifortex_test_task/repository/SessionRepository.java
+++ src/main/java/com/example/java_ifortex_test_task/repository/SessionRepository.java
@@ -1,17 +1,38 @@
 package com.example.java_ifortex_test_task.repository;
 
-import com.example.java_ifortex_test_task.entity.DeviceType;
 import com.example.java_ifortex_test_task.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
-    @Query(value = "", nativeQuery = true)
-    Session getFirstDesktopSession(DeviceType deviceType);
+    @Query(value = """
+    SELECT s.id,
+           s.user_id,
+           s.started_at_utc,
+           s.ended_at_utc,
+           (s.device_type - 1) AS device_type
+    FROM sessions s
+    WHERE device_type = ?1
+    ORDER BY started_at_utc
+    LIMIT 1
+    """, nativeQuery = true)
+    Session getFirstDesktopSession(int deviceTypeCode);
 
-    @Query(value = "", nativeQuery = true)
-    List<Session> getSessionsFromActiveUsersEndedBefore2025(LocalDateTime endDate);
+    @Query(value = """
+    SELECT s.id,
+           s.user_id,
+           s.started_at_utc,
+           s.ended_at_utc,
+           (s.device_type - 1) AS device_type
+    FROM sessions s
+             JOIN users u ON s.user_id = u.id
+    WHERE u.deleted = false
+    AND s.ended_at_utc IS NOT NULL
+    AND s.ended_at_utc < ?1
+    ORDER BY s.started_at_utc DESC
+    """, nativeQuery = true)
+    List<Session> getSessionsFromActiveUsersEndedBefore2025(Timestamp endTimestamp);
 }
